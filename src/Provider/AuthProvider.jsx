dd47862e8c8a64b9e996a9733aa2103aba2d92cb
wolfiee42 <saifalislam2022@gmail.com>
2024-01-09 15:47:01 +0600

    created auth context for user

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+
+    
+
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './Provider/AuthProvider.jsx';
 
 
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>,
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>,
 )
