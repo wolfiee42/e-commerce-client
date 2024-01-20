5587f9f4a4a87ad72060346964184d0162d5b81d
wolfiee42 <saifalislam2022@gmail.com>
2024-01-20 10:26:23 +0600

    route cleared

--- src/components/Product section/ProductSection.jsx
+++ src/components/Product section/ProductSection.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import ProductSampleSingle from "../../layouts/Home/ProductSampleSingle";
 import Cover from "../Cover/Cover";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const ProductSection = ({items, img, title}) => {
+const ProductSection = ({ items, img, title }) => {
+    const [route, setroute] = useState()
+    useEffect(() => {
+        items && items.map(item => setroute(item.classification))
+    })
+    console.log(route);
     return (
         <div>
             {title && <Cover img={img} title={title} />}
@@ -13,9 +19,9 @@ const ProductSection = ({items, img, title}) => {
                 }
             </div>
             {!title ? "" : <div className=" w-[150px] mx-auto my-5">
-                <button className="uppercase btn btn-xs md:btn-sm lg:btn bg-[#636940] lg:bg-[#636940] text-white lg:text-white border-b-2 lg:border-b-2 border-[#474A2C] lg:border-[#474A2C] lg:hover:bg-[#555A36] hover:border-none lg:hover:border-none"><Link to={`/order`}>Order Now</Link></button>
+                <button className="uppercase btn btn-xs md:btn-sm lg:btn bg-[#636940] lg:bg-[#636940] text-white lg:text-white border-b-2 lg:border-b-2 border-[#474A2C] lg:border-[#474A2C] lg:hover:bg-[#555A36] hover:border-none lg:hover:border-none"><Link to={`/order?category=${route}`}>Order Now</Link></button>
             </div>
-                
+
             }
         </div>
     );
--- src/layouts/Home/ProductSampleSingle.jsx
+++ src/layouts/Home/ProductSampleSingle.jsx
@@ -2,7 +2,7 @@
 const ProductSampleSingle = ({ product }) => {
     // eslint-disable-next-line react/prop-types
     const { image, price, name, desc } = product;
-    console.log(product);
+    
     return (
         <div className="flex justify-around md:justify-between items-center lg:items-start  lg:w-[550px] lg:h-[130px] p-1 md:p-3 lg:p-5 bg-[#E0F5EB] rounded-lg md:mx-auto">
             <img src={image} className="w-16 h-16 lg:w-24 lg:h-24 rounded-lg" alt="" />
